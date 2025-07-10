8487539775612b4e695286b927afbe78034eff9d
Sever Ali <s.elruwanduzi@gmail.com>
2025-07-10 12:07:25 +0100

    Included prometheus monitoring and can later set up Grafana

--- run.py
+++ run.py
@@ -1,6 +1,18 @@
-from app import create_app
+from flask import Flask
+from prometheus_client import Counter, generate_latest
 
-app = create_app()
+app = Flask(__name__)
+
+REQUEST_COUNT = Counter('request_count', 'Total number of requests')
+
+@app.route('/')
+def home():
+    REQUEST_COUNT.inc()
+    return "Hello Wiki App!"
+
+@app.route('/metrics')
+def metrics():
+    return generate_latest(), 200, {'Content-Type': 'text/plain'}
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
